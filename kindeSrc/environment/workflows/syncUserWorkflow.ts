a7b899bb6bf4ceda71133e7a848b25930ad3f326
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-08-15 18:14:37 +0300

    remove: kinde.fetch binding for test

--- kindeSrc/environment/workflows/syncUserWorkflow.ts
+++ kindeSrc/environment/workflows/syncUserWorkflow.ts
@@ -16,7 +16,6 @@ export const workflowSettings: WorkflowSettings = {
     trigger: WorkflowTrigger.PostAuthentication,
     bindings: {
         "kinde.env": {},
-        "kinde.fetch": {},
         url: {},
     },
 };
