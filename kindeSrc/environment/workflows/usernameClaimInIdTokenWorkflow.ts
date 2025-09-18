e940e2c70bf21e52ab4a695eba5c413af1e5d2a1
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-09-18 14:58:03 +0300

    feat(workflows): integrate Kinde API to fetch org details and user roles

--- kindeSrc/environment/workflows/usernameClaimInIdTokenWorkflow.ts
+++ kindeSrc/environment/workflows/usernameClaimInIdTokenWorkflow.ts
@@ -2,8 +2,23 @@ import {
     onUserTokenGeneratedEvent,
     WorkflowSettings,
     WorkflowTrigger,
+    createKindeAPI,
     idTokenCustomClaims,
 } from "@kinde/infrastructure";
+
+// This workflow requires you to set up the Kinde management API
+// You can do this by going to the Kinde dashboard.
+//
+// Create an M2M application with the following scopes enabled:
+// * read:organizations
+// * read:organization_user_roles
+//
+// In Settings -> Environment variables set up the following variables with the
+// values from the M2M application you created above:
+//
+// * KINDE_WF_M2M_CLIENT_ID
+// * KINDE_WF_M2M_CLIENT_SECRET - Ensure this is setup with sensitive flag
+// enabled to prevent accidental sharing
 
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
@@ -26,9 +41,19 @@ export default async function Workflow(event: onUserTokenGeneratedEvent) {
 
     // idToken.username = "";
 
-    const userId = event.context.user.id;
-    console.log("userId: ", userId);
+    const kindeAPI = await createKindeAPI(event);
 
     const orgCode = event.context.organization.code;
-    console.log("orgCode: ", orgCode);
+    const { data: orgDetails } = await kindeAPI.get({
+        endpoint: `organizations/${orgCode}`,
+    });
+    const orgName = orgDetails.name;
+    console.log("orgName: ", orgName);
+
+    const userId = event.context.user.id;
+    const { data: roleDetails } = await kindeAPI.get({
+        endpoint: `organizations/${orgCode}/users/${userId}/roles`,
+    });
+    const userRoles = roleDetails.roles;
+    console.log("userRoles: ", userRoles);
 }
