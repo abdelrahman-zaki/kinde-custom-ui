a6ff27ecb7c85b841da03a0c434e83dfa04cc5d6
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-10-07 13:29:06 +0300

    feat: add preRegistration workflow (initial setup with event log)

---
+++ kindeSrc/environment/workflows/preUserRegistrationWorkflow.ts
@@ -0,0 +1,25 @@
+import {
+    onUserPreRegistrationEvent,
+    WorkflowSettings,
+    denyAccess,
+} from "@kinde/infrastructure";
+
+// The settings for this workflow
+export const workflowSettings: WorkflowSettings = {
+    id: "preRegistration",
+    name: "Block empty org users",
+    failurePolicy: {
+        action: "stop",
+    },
+    trigger: "user:pre_registration",
+    bindings: {
+        "kinde.auth": {},
+    },
+};
+
+// The workflow code to be executed when the event is triggered
+export default async function Workflow(
+    event: onUserPreRegistrationEvent
+) {
+    console.log("handlePreRegistration", event);
+}
